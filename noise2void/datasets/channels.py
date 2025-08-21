237aaf5ef00ff83537b726e114ddcbdb1269c386
William Thornley <william.thornley@manchester.ac.uk>
2025-08-21 12:10:12 +0100

    Refactor Tungsten dataset to include mag-filtering, debug statistics, channel filtering and a blacklist.

--- noise2void/datasets/channels.py
+++ noise2void/datasets/channels.py
@@ -4,6 +4,9 @@ A type for HAADF, LAADF and BF channels in STEM.
 """
 
 from enum import Enum
+from pathlib import Path
+
+import hyperspy.api as hs
 
 
 class Channel(Enum):
@@ -12,3 +15,51 @@ class Channel(Enum):
     BF = "BF"
     LAADF = "LAADF"
     HAADF = "HAADF"
+
+
+class MultiChannelMetadata:
+
+    def __init__(
+        self, fpaths: dict[Channel, Path], sample: str, index: int,
+    ):
+        assert len(fpaths) > 0
+        self.fpaths = fpaths
+        self.sample = sample
+        self.index = index
+        self.px_scale = None
+        self.shape = None
+
+    def fetch_scale_shape(self):
+        an_fpath = self.an_fpath()
+        sig = hs.load(str(an_fpath))  # Load any image
+        px_scale, px_units = sig.axes_manager[-1].scale, sig.axes_manager[-1].units
+        if px_units == "Å":  # Everything in nanometres
+            px_scale /= 10.
+            px_units = "nm"
+        elif px_units == "um":
+            px_scale *= float(1E3)
+            px_units = "nm"
+        assert px_units == "nm"
+        assert sig.data.shape[-2] == sig.data.shape[-1]
+        self.px_scale = px_scale
+        self.shape = sig.data.shape[-1]
+
+    def has_channels(self, channels: set[Channel]) -> bool:
+        return channels.issubset(set(self.fpaths.keys()))
+
+    def is_blacklisted(self, blacklist: dict[str, list[int]]) -> bool:
+        """Expects the blacklist to be a dict, indexed by sample-name, with each item containing a list of indices
+        that should be excluded."""
+        if self.sample in blacklist.keys():
+            if self.index in blacklist[self.sample]:
+                return True
+        return False
+
+    def in_scale_range(self, scale: float, factor: float) -> bool:
+        """Scale is pixel-size in nanometres, and the permitted range is from 1/factor to factor times the scale."""
+        if scale / factor < self.px_scale < scale * factor:
+            return True
+        return False
+
+    def an_fpath(self) -> Path:
+        return next(iter(self.fpaths.values()))
--- noise2void/datasets/tungsten_dataset.py
+++ noise2void/datasets/tungsten_dataset.py
@@ -2,12 +2,17 @@
 
 Contains the PyTorch Dataset for the Tunsten Disulphide experimental data.
 
+The dataset contains data from three experimental sessions, divided into five samples. Each image contains HAADF and
+BF channels, and sometimes LAADF too. Some images are of very low magnification, some of an intermediate mag and most
+are at high magnification. Most images are 2K or 4K, although a few are 1K.
+
 Note: to delete all generated PNGs, navigate to the Tungsten WS2 data directory and use this command
 `find . -type f -regex ".*\.png" -not -name "1777865_LAADF_fft.png" -delete`
 """
 
-import re
+import tomllib
 from pathlib import Path
+from dataclasses import dataclass
 
 import torch
 from torch.utils.data import Dataset, IterableDataset
@@ -20,120 +25,146 @@ from matplotlib_scalebar.scalebar import ScaleBar
 import hyperspy.api as hs
 from tqdm import tqdm
 
-from channels import Channel
-
+from channels import Channel, MultiChannelMetadata
 
 
 class TungstenDataset:
     """Abstraction over the twisted WS2 dataset"""
 
     DATA_DIRECTORY = Path("data/Twisted WS2/raw_images")
+    BLACKLIST_FILE = Path("noise2void/datasets/tungsten_dataset_blacklist.txt")
 
-    def __init__(self, image_size: int, channels: list[Channel]):
+    def __init__(self, image_size: int, channels: list[Channel], px_scale: float):
+        """
+        Parameters
+        ----------
+        image_size: int
+            The size of each image in pixels. Outputs will be randomly cropped
+        channels: list[Channel]
+            The channels that must be included with each image
+        px_scale: float
+            The pixel size, in nanometres. Images with similar `px_scale` will be interpolated to match this value
+        """
+        print("Initialising Dataset")
         self.channels = channels
         self.image_size = image_size
-        self.sample_filegroups = self._find_filepaths()
+        self.px_scale = px_scale
+        print("Finding filepaths")
+        self._all_sample_filegroups = self._find_filepaths()
 
-    def plot_images(self):
+        with open(self.BLACKLIST_FILE, 'rb') as file:
+            blacklist = tomllib.load(file)
+        self.samples = set(blacklist.keys())
+
+        print("Applying blacklist")
+        self._valid_sample_filegroups = list(filter(
+            lambda meta: meta.is_blacklisted(blacklist),
+            self._all_sample_filegroups
+        ))
+
+        print("Filtering by channels")
+        self._channel_sample_filegroups = list(filter(
+            lambda meta: meta.has_channels(channels),
+            self._valid_sample_filegroups
+        ))
+
+        print("Fetching scales and shapes")
+        for meta in self._channel_sample_filegroups:
+            meta.fetch_scale_shape()
+
+        print("Filtering by scale")
+        self._to_scale_sample_filegroups = list(filter(
+            lambda meta: meta.in_scale_range(self.px_scale, 3.),
+            self._channel_sample_filegroups
+        ))
+
+        self.sample_filegroups = self._to_scale_sample_filegroups
+
+    def _plot_images(self):
         """Converts each multi-channel image to a PNG"""
 
         with tqdm(total=193, desc="Plotting multi-channel images") as pbar:
 
-            for sample in self.sample_filegroups:
-                for im_index in self.sample_filegroups[sample]:
+            for meta in self._all_sample_filegroups:
+                a_chan = next(iter(meta.fpaths.keys()))
+                savepath = meta.fpaths[a_chan].parent / \
+                    (meta.fpaths[a_chan].stem.strip(a_chan.value + "_") + ".png")
+                if savepath.exists():
+                    continue
 
-                    a_chan = next(iter(self.sample_filegroups[sample][im_index].keys()))
-                    savepath = self.sample_filegroups[sample][im_index][a_chan].parent / \
-                               (self.sample_filegroups[sample][im_index][a_chan].stem.strip(a_chan.value + "_") + ".png")
-                    if savepath.exists():  # Already plotted...
-                        continue
+                chans = sorted(meta.fpaths.keys(), key=lambda chan: chan.value)
+                assert len(chans) > 0
+                gridspec_kw = dict(left=0, right=1, bottom=0, top=0.91, hspace=0, wspace=0)
+                fig, axes = plt.subplots(
+                    1, len(chans), figsize=(len(chans) * 8, 8 * 1.1), gridspec_kw=gridspec_kw
+                )
+                for ax_index, chan in enumerate(chans):
+                    sig = hs.load(str(meta.fpaths[chan]))
+                    axes[ax_index].imshow(sig.data, cmap="inferno", interpolation=None)
+                    axes[ax_index].set_title(chan.value)
+                    axes[ax_index].axis("off")
+                sbar = ScaleBar(
+                    meta.px_scale, "nm", location="lower right", color='w', box_color='k', box_alpha=0.7
+                )
+                axes[ax_index].add_artist(sbar)
+                fig.savefig(savepath, dpi=210)
+                plt.close(fig)
+                pbar.update()
 
-                    chans = sorted(self.sample_filegroups[sample][im_index].keys(), key=lambda chan: chan.value)
-                    assert len(chans) > 0
-                    gridspec_kw = dict(left=0, right=1, bottom=0, top=0.91, hspace=0, wspace=0)
-                    fig, axes = plt.subplots(
-                        1, len(chans), figsize=(len(chans) * 8, 8 * 1.1), gridspec_kw=gridspec_kw
-                    )
-                    for ax_index, chan in enumerate(chans):
-                        sig = hs.load(self.sample_filegroups[sample][im_index][chan])
-                        axes[ax_index].imshow(sig.data, cmap="inferno", interpolation=None)
-                        axes[ax_index].set_title(chan.value)
-                        axes[ax_index].axis("off")
-                    px_scale, px_units = sig.axes_manager[-1].scale, sig.axes_manager[-1].units
-                    if px_units == "Å":
-                        px_scale /= 10.
-                        px_units = "nm"
-                    sbar = ScaleBar(
-                        px_scale, px_units, location="lower right", color='w', box_color='k', box_alpha=0.7
-                    )
-                    axes[ax_index].add_artist(sbar)
-                    fig.savefig(savepath, dpi=210)
-                    plt.close(fig)
-                    pbar.update()
-
-
-    def _print_samplegroup_stats(self):
+    def print_dataset_stats(self):
         """Prints sample-wise details about the samples' images, including image sizes and channels used."""
 
         # Count channels and ensure consistency
         print("\n\n#       Channels       #")
-        for sample in self.sample_filegroups:
+        for sample in self.samples:
             print(f"{sample:-^16}")
             sample_channels: set[Channel] | None = None
-            for im_index in self.sample_filegroups[sample]:
-                chans = set(self.sample_filegroups[sample][im_index].keys())
-                if sample_channels is None:
+            for meta in filter(lambda meta: meta.sample == sample, self._all_sample_filegroups):
+                chans = set(meta.fpaths.keys())
+                if  sample_channels is None:
                     sample_channels = set(chans)
                 else:
-                    fpath = next(iter(self.sample_filegroups[sample][im_index].values()))  # One filepath to help debug
+                    an_fpath = meta.an_fpath()
                     if len(sample_channels) > len(chans):
                         missing_channels = sample_channels - chans
-                        print(f"Warning: channel count mismatch for {fpath.name}, missing {missing_channels}")
+                        print(f"Warning: channel count mismatch for {an_fpath.name}, missing {missing_channels}")
                     elif len(sample_channels) < len(chans):
                         extra_channels = chans - sample_channels
-                        print(f"Warning: channel count mismatch for {fpath.name}, extra {extra_channels}")
+                        print(f"Warning: channel count mismatch for {an_fpath.name}, extra {extra_channels}")
             print(f"Sample channels: {sample_channels}")
 
         print("\n\n#       Image shapes       #")
-        for sample in self.sample_filegroups:
+        for sample in self.samples:
             print(f"{sample:-^16}")
             image_shapes: list[int] = list()
-            for im_index in self.sample_filegroups[sample]:
-                an_fpath = next(iter(self.sample_filegroups[sample][im_index].values()))
-                sig = hs.load(str(an_fpath))  # Load any image
-                im_shape = sig.data.shape
-                if len(im_shape) != 2:
-                    print(f"Warning: unexpected image shape for {an_fpath.name} {im_shape=}")
-                    continue
-                if im_shape[0] != im_shape[1]:
-                    print(f"Warning: image is not square for {an_fpath.name} {im_shape=}")
-                    continue
-                image_shapes.append(im_shape[-1])
+            for meta in filter(lambda meta: meta.sample == sample, self._all_sample_filegroups):
+                an_fpath = meta.an_fpath()
+                im_shape = meta.shape
+                image_shapes.append(im_shape)
             im_sizes, size_frequencies = np.unique(image_shapes, return_counts=True)
             print(f"Found image sizes: {im_sizes}")
             print(f"Frequencies: {size_frequencies}")
 
-    @staticmethod
-    def _try_insert(
-            sample_filegroups: dict[str, dict[int, dict[Channel, Path]]], sample: str, chan: Channel, index: int,
-            filepath: Path
-    ) -> bool:
-        """Inserts the file path into the directory structure, or returns false if there is a problem and prints it"""
+        print("\n\n    Dataset sizes    ")
+        print(f"Valid images: {len(self._valid_sample_filegroups)}")
+        print(f"Images satisfying channels {self.channels}: {len(self._channel_sample_filegroups)}")
+        print(f"Images satisfying mag constraints: {len(self._to_scale_sample_filegroups)}")
+        print(f"Total images used: {len(self.sample_filegroups)}")
+        print(f"Effective number of {self.image_size}px images in dataset (after mag interpolation): {self._calculate_effective_length()}")
 
-        if sample_filegroups[sample].get(index) is None:  # New index entry
-            sample_filegroups[sample][index] = {chan: filepath}
-        else:
-            if sample_filegroups[sample][index].get(chan) is None:  # New channel (expected)
-                sample_filegroups[sample][index][chan] = filepath
-            else:  # Duplicate image?!
-                warn = f"Found duplicate channel for image {filepath} and " + \
-                       f"{sample_filegroups[sample][index][chan]}"
-                print(warn)
-                return False
-        return True
+    def _calculate_effective_length(self) -> float:
+        """Calculates the effective length of the dataset, taking into account interpolation and tiled cropping"""
+
+        total = 0.0
+        for meta in self.sample_filegroups:
+            interp_factor = meta.scale / self.px_scale
+            new_shape = interp_factor * meta.shape
+            effective_images = (new_shape / self.image_size) ** 2.
+            total += effective_images
+        return total
 
     @classmethod
-    def _find_filepaths(cls) -> dict[str, dict[int, dict[Channel, Path]]]:
+    def _find_filepaths(cls) -> list[MultiChannelMetadata]:
         """Constructs the dictionary of filepaths in the experimental dataset
 
         Returns
@@ -163,7 +194,9 @@ class TungstenDataset:
                     continue
                 chan = Channel[chan]
                 index = int(index)
-                if not cls._try_insert(sample_filegroups, sample, chan, index, fpath):
+                try:
+                    cls._try_insert_filepath(sample_filegroups, sample, chan, index, fpath)
+                except ValueError:
                     continue
 
         sample_filegroups["May25_tWS2_4"] = dict()
@@ -177,7 +210,9 @@ class TungstenDataset:
             index = int(index)
             if mag[-2:] == "kX" or mag[-2:] == "KX":
                 continue  # Too low mag to be useful
-            if not cls._try_insert(sample_filegroups, "May25_tWS2_4", chan, index, fpath):
+            try:
+                cls._try_insert_filepath(sample_filegroups, "May25_tWS2_4", chan, index, fpath)
+            except ValueError:
                 continue
 
         for sample in ("Jul24_tWS2_1", "Jul24_tWS2_3"):  # These two are organised the same
@@ -194,15 +229,42 @@ class TungstenDataset:
                     continue  # Too low mag to be useful
                 if float(mag[:-2]) < 1.8:  # Too low mag to be useful
                     continue
-                if not cls._try_insert(sample_filegroups, sample, chan, index, fpath):
+                try:
+                    cls._try_insert_filepath(sample_filegroups, sample, chan, index, fpath)
+                except ValueError:
                     continue
 
-        return sample_filegroups
+        sample_filegroups_list = list()
+        for sample in sample_filegroups:
+            for im_index in sample_filegroups[sample]:
+                sample_filegroups_list.append(
+                    MultiChannelMetadata(sample_filegroups[sample][im_index], sample, im_index)
+                )
+
+        return sample_filegroups_list
+
+    @staticmethod
+    def _try_insert_filepath(
+        sample_filegroups: dict[str, dict[int, dict[Channel, Path]]], sample: str, chan: Channel, index: int,
+        fpath: Path
+    ):
+        """Attempts to insert an item into the filegroup-dict, raising a ValueError if a duplicate item is already
+        present in the structure."""
+
+        if sample_filegroups.get(sample) is None:
+            sample_filegroups[sample] = dict()
+        if sample_filegroups[sample].get(index) is None:
+            sample_filegroups[sample][index] = dict()
+        if sample_filegroups[sample][index].get(chan) is None:  # This should not be populated yet
+            sample_filegroups[sample][index][chan] = fpath
+        else:
+            warn = f"An image with index {index} and channel {chan} from {sample} already exists:"
+            warn += f"\t{fpath}\n\t{sample_filegroups[sample][index][chan]}"
+            raise ValueError(warn)
 
 
 if __name__ == "__main__":
     # For getting info on the samples and plotting etc.
 
-    dset = TungstenDataset(256, list())
-    print(f"{dset.total_images}")
-    dset.plot_images()
+    dset = TungstenDataset(256, [Channel.HAADF, Channel.BF], 0.007)
+    dset.print_dataset_stats()
