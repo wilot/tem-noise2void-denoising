72775d9a74edc748da86cacbddc7c94afbccf83c
William Thornley <william.thornley@manchester.ac.uk>
2026-05-13 15:35:21 +0100

    Config modifications for IrGLCs

--- noise2void/datasets/generators.py
+++ noise2void/datasets/generators.py
@@ -4,17 +4,7 @@ Code to generate the datasets from config files.
 """
 
 from noise2void.datasets.channels import Channel
-from noise2void.datasets.tungsten_dataset import TungstenDataset
 from noise2void.datasets.iridium_glc_dataset import IridiumVideoDataset
-
-
-def _generate_tungsten_from_config(config, predict: bool) -> TungstenDataset:
-    """Generates a Tungsten dataset with the appropriate configuration"""
-
-    channels = [Channel(chan) for chan in config.channels]
-    return TungstenDataset(
-        config.image_size, channels, config.dataset.px_scale, config.dataset.example_index
-    )
 
 
 def _generate_iridium_video_from_config(config, predict: bool) -> IridiumVideoDataset:
@@ -33,6 +23,5 @@ def _generate_iridium_video_from_config(config, predict: bool) -> IridiumVideoDa
 
 # A mapping from the string specified in every config file's dataset section to a method to generate it from that config
 dataset_generators = {
-    "WS2": _generate_tungsten_from_config,
     "IridiumGLCVideo": _generate_iridium_video_from_config,
 }
