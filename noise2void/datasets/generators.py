95a28ab6dbf8d0b06f940705a5e28deb42264eb7
William Thornley <william.thornley@manchester.ac.uk>
2026-05-13 15:00:11 +0100

    Restore iridium_glc_dataset dataloader from history

--- noise2void/datasets/generators.py
+++ noise2void/datasets/generators.py
@@ -13,18 +13,12 @@ def _generate_tungsten_from_config(config, predict: bool) -> TungstenDataset:
 
     channels = [Channel(chan) for chan in config.channels]
     return TungstenDataset(
-        config.image_size, channels, config.dataset.px_scale,
-        config.dataset.example_index if config.dataset.example_index != "none" else None
+        config.image_size, channels, config.dataset.px_scale, config.dataset.example_index
     )
 
 
 def _generate_iridium_video_from_config(config, predict: bool) -> IridiumVideoDataset:
-    """Generates an IridiumDataset from config."""
-
-    # This dataset only has the HAADF channel!
-    channels = [Channel(chan) for chan in config.channels]
-    if len(channels) > 1 or channels[0] != Channel.HAADF:
-        raise ValueError(f"Only HAADF channel is valid for IrVideo dataset, not {channels=}")
+    """Generates an IridiumDataset from config. If predict is set, the video filter is ignored (e.g. for inference)."""
 
     try:  # In older config file versions, this was None
         video_filter = config.dataset.video_filter
@@ -33,12 +27,12 @@ def _generate_iridium_video_from_config(config, predict: bool) -> IridiumVideoDa
     if video_filter == "none":
         video_filter = None
     return IridiumVideoDataset(
-        config.image_size, config.dataset.example_index, video_filter
+        config.image_size, config.dataset.example_index, None if predict else video_filter
     )
 
 
 # A mapping from the string specified in every config file's dataset section to a method to generate it from that config
 dataset_generators = {
     "WS2": _generate_tungsten_from_config,
-    "IridiumVideo": _generate_iridium_video_from_config,
+    "IridiumGLCVideo": _generate_iridium_video_from_config,
 }
